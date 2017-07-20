5656710b53652b6544ff49e8630056178df0f390
AslanDeli <alanguzek@gmail.com>
2017-07-20 13:01:39 +0200

    Update test.py
    Lepszy, bardziej przejrzysty kod

--- test.py
+++ test.py
@@ -1,29 +1,27 @@
-import random
 import os
-import sysconfig
+import sys
 
 print("KALKULATOR")
-n1 = input("Podaj pierwsza liczbe")
-n2 = input ("Podaj drugą liczbe")
-wyb = input ("1.DODAWANIE  2.ODEJMOWANIE  3.MNOŻENIE 4. DZIELENIE")
+n1 = input("Podaj pierwsza liczbe: ")
+n2 = input ("Podaj drugą liczbe: ")
+wyb = input (" 1.DODAWANIE\n 2.ODEJMOWANIE\n 3.MNOŻENIE\n 4.DZIELENIE\n ")
 num1=int(n1)
 num2=int(n2)
 def calc(wyb):
-    suma = wyb
+    wynik = "ERROR"
     if wyb=="1":
-        suma = num1 + num2
+        wynik = num1 + num2
     elif wyb=="2":
-        suma = num1 - num2
+        wynik = num1 - num2
     elif wyb=="3":
-        suma = num1*num2
+        wynik = num1*num2
     elif wyb=="4":
-        suma = num1 / num2
-    else:
-        suma = "ERROR"
+        wynik = num1 / num2
+        
     print(suma)
 
 
 calc(wyb)
 
-xD = input("heelo")
+xD = input("Komentarz: ")
 print (xD)
